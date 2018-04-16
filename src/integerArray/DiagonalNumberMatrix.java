746c63305c183950fb6c0e8f8c098befd1b8c47d
Ruifeng Ma <ruifengm@sg.ibm.com>
2018-04-16 17:40:49 +0800

    Optimized code.

--- src/integerArray/DiagonalNumberMatrix.java
+++ src/integerArray/DiagonalNumberMatrix.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 /**
  * Print a matrix of given size where numbers are diagonally filled up.
  * 
+ * E.g. A 3X3 matrix should look like below. 
+ * 
+ * 
+ * 
  * @author ruifengm
  * @2018-Apr-16
  */
@@ -17,44 +21,40 @@ public class DiagonalNumberMatrix {
 		while (k < m*n) {
 			if (i == 0 || j == n-1) { // fill up diagonal from top right to bottom left
 				while (j>0 && i<m-1) {
-					matrix[i][j] = k;
-					k++;
+					matrix[i][j] = k++;
 					i++; 
 					j--; 
 				}
 				if (j==0 && i<m-1) { // hit first column but not the last row
-					matrix[i][j] = k;
-					k++;
+					matrix[i][j] = k++;
 					i++; // move down
-				} else {
-					matrix[i][j] = k;
-					k++;
+				} else if (i == m-1) {
+					matrix[i][j] = k++;
 					j++; // move right
 				}
 			}
 			if (j == 0 || i == m-1) { // fill up diagonal from bottom left to top right
 				while (i>0 && j<n-1) {
-					matrix[i][j] = k;
-					k++;
+					matrix[i][j] = k++; 
 					i--; 
 					j++; 
 				}
 				if (i==0 && j<n-1) { // hit first row but not the last column
-					matrix[i][j] = k;
-					k++;
+					matrix[i][j] = k++;
 					j++; // move right
-				} else {
-					matrix[i][j] = k;
-					k++;
+				} else if (j==n-1) {
+					matrix[i][j] = k++;
 					i++; // move down
-				}
+				}  
 			}
 		}
 		return matrix;
 	}
 
 	public static void main(String[] args) {
+		//int[][] matrix = printMatrix(3, 3);
 		int[][] matrix = printMatrix(4, 5);
+		//int[][] matrix = printMatrix(5, 4);
 		for (int[] row: matrix) System.out.println(Arrays.toString(row));
 	}
 }
